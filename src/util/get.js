15cbd72371204dec22ca35fc1eaaccaf6e036fdd
neeschit <nranganath@cargurus.com>
2020-02-08 17:46:16 -0500

    add retry logic to https get

--- src/util/get.js
+++ src/util/get.js
@@ -1,27 +1,41 @@
 const https = require("https");
+
+const logError = ({ url, count, reject, response }) => {
+    console.error(url);
+    console.error(count);
+    return reject(new Error("statusCode=" + response.statusCode));
+};
+
 const get = url => {
     return new Promise((resolve, reject) => {
-        https.get(url, response => {
-            if (response.statusCode === 404) {
-                return resolve(null);
-            }
-            if (response.statusCode < 200 || response.statusCode >= 300) {
-                return reject(new Error("statusCode=" + response.statusCode));
-            }
-            var body = [];
-            response.on("data", function(chunk) {
-                body.push(chunk);
-            });
-
-            response.on("end", function() {
-                try {
-                    body = JSON.parse(Buffer.concat(body).toString());
-                } catch (e) {
-                    reject(e);
+        let count = 0;
+        const retry = () => {
+            https.get(url, response => {
+                console.log(url);
+                count++;
+                if (response.statusCode === 500 && count <= 3) {
+                    retry();
+                    return;
                 }
-                resolve(body);
+                if (response.statusCode < 200 || response.statusCode >= 300) {
+                    return logError({ url, response, reject, count });
+                }
+                let body = [];
+                response.on("data", function(chunk) {
+                    body.push(chunk);
+                });
+
+                response.on("end", function() {
+                    try {
+                        body = JSON.parse(Buffer.concat(body).toString());
+                    } catch (e) {
+                        reject(e);
+                    }
+                    resolve(body);
+                });
             });
-        });
+        };
+        retry();
     });
 };
 
