ed8a3cd83e7839d14e730d22a197dd3e906d6b44
neeschit <nischit14@gmail.com>
2020-02-18 00:38:45 -0500

    log timestamp

--- src/strategy/narrowRangeBar.ts
+++ src/strategy/narrowRangeBar.ts
@@ -185,7 +185,7 @@ export class NarrowRangeBarStrategy {
         const bar = lastBar.find(bar => bar.t === entryBarTimestamp.getTime());
 
         if (!bar) {
-            console.error("couldnt find appropriate bar", lastBar);
+            console.error("couldnt find appropriate bar", entryBarTimestamp);
             return null;
         }
 
