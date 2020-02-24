04e4b6d1f30150f72d5d692610ca17dbdeee873b
neeschit <nischit14@gmail.com>
2020-02-24 00:14:00 -0500

    closes #10

--- src/connection/alpaca.ts
+++ src/connection/alpaca.ts
@@ -3,8 +3,12 @@ import * as dotenv from "dotenv";
 
 const config = dotenv.config().parsed;
 
+const API_KEY = (config && config.ALPACA_SECRET_KEY_ID) || process.env.ALPACA_SECRET_KEY_ID || "";
+
+const SECRET_KEY = (config && config.ALPACA_SECRET_KEY) || process.env.ALPACA_SECRET_KEY || "";
+
 export const alpaca = new Alpaca({
-    keyId: config!.ALPACA_SECRET_KEY_ID,
-    secretKey: config!.ALPACA_SECRET_KEY,
+    keyId: API_KEY,
+    secretKey: SECRET_KEY,
     paper: true
 });
