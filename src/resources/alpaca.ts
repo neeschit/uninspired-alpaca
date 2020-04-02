426ca95639e73aab68766f77d24b9f84b9d2fc17
neeschit <nischit14@gmail.com>
2020-04-02 00:08:43 -0400

    remove dangerous change in alpaca connection to live

--- src/resources/alpaca.ts
+++ src/resources/alpaca.ts
@@ -3,12 +3,12 @@ import * as dotenv from "dotenv";
 
 const config = dotenv.config().parsed;
 
-const API_KEY = (config && config.LIVE_SECRET_KEY_ID) || process.env.LIVE_SECRET_KEY_ID || "";
+const API_KEY = (config && config.ALPACA_SECRET_KEY_ID) || process.env.ALPACA_SECRET_KEY_ID || "";
 
-const SECRET_KEY = (config && config.LIVE_SECRET_KEY) || process.env.LIVE_SECRET_KEY || "";
+const SECRET_KEY = (config && config.ALPACA_SECRET_KEY) || process.env.ALPACA_SECRET_KEY || "";
 
 export const alpaca = new Alpaca({
     keyId: API_KEY,
     secretKey: SECRET_KEY,
-    paper: false
+    paper: true
 });
