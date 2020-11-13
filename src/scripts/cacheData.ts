8166a0769a0855402415fff87548644c0002fc05
neeschit <nischit14@gmail.com>
2020-11-12 21:27:24 -0500

    fix data caching script to account for time of day

--- src/scripts/cacheData.ts
+++ src/scripts/cacheData.ts
@@ -52,32 +52,30 @@ async function run(duration = DefaultDuration.one, period = PeriodType.minute) {
 
     const hasDayEnded = isAfterMarketClose(Date.now());
 
-    if (hasDayEnded) {
-        for (const symbol of companies) {
-            const endDate = endOfDay(addDays(Date.now(), 0));
-            for (
-                let date = startDate;
-                date.getTime() < endDate.getTime();
-                date = addBusinessDays(date, 90)
-            ) {
-                const end = addBusinessDays(date, 90);
-                const daysMinutes = await getPolyonData(
-                    symbol,
-                    date,
-                    end.getTime() > endDate.getTime() ? endDate : end,
-                    PeriodType.day,
-                    DefaultDuration.one
-                );
+    for (const symbol of companies) {
+        const endDate = endOfDay(addDays(Date.now(), hasDayEnded ? 0 : -1));
+        for (
+            let date = startDate;
+            date.getTime() < endDate.getTime();
+            date = addBusinessDays(date, 90)
+        ) {
+            const end = addBusinessDays(date, 90);
+            const daysMinutes = await getPolyonData(
+                symbol,
+                date,
+                end.getTime() > endDate.getTime() ? endDate : end,
+                PeriodType.day,
+                DefaultDuration.one
+            );
 
-                if (!daysMinutes[symbol] || !daysMinutes[symbol].length) {
-                    continue;
-                }
+            if (!daysMinutes[symbol] || !daysMinutes[symbol].length) {
+                continue;
+            }
 
-                try {
-                    await batchInsertDailyBars(daysMinutes[symbol], symbol);
-                } catch (e) {
-                    LOGGER.error(`Error inserting for ${symbol}`, e);
-                }
+            try {
+                await batchInsertDailyBars(daysMinutes[symbol], symbol);
+            } catch (e) {
+                LOGGER.error(`Error inserting for ${symbol}`, e);
             }
         }
     }
