f5d03195f1262626f5569c91525778f93823037c
neeschit <nischit14@gmail.com>
2021-04-12 10:22:21 -0400

    add credentials to fix issues with permissions when running in the cloud

--- functions/cache-orchestrator/createCredentials.sh
+++ functions/cache-orchestrator/createCredentials.sh
@@ -2,3 +2,4 @@ gcloud iam service-accounts create cachingaccount --display-name "Cache Data Acc
 gcloud projects add-iam-policy-binding fleet-tractor-309018 --member serviceAccount:cachingaccount@fleet-tractor-309018.iam.gserviceaccount.com --role roles/cloudfunctions.invoker
 gcloud projects add-iam-policy-binding fleet-tractor-309018 --member serviceAccount:cachingaccount@fleet-tractor-309018.iam.gserviceaccount.com --role roles/logging.bucketWriter
 gcloud projects add-iam-policy-binding fleet-tractor-309018 --member serviceAccount:cachingaccount@fleet-tractor-309018.iam.gserviceaccount.com --role roles/storage.objectAdmin
+gcloud projects add-iam-policy-binding fleet-tractor-309018 --member serviceAccount:cachingaccount@fleet-tractor-309018.iam.gserviceaccount.com --role roles/pubsub.publisher
