4377b9e19506b077b7991784ab274456d78135db
neeschit <nischit14@gmail.com>
2021-04-25 23:48:22 -0400

    inital code to get app engine deployment going

---
+++ appengine/trading-service/index.ts
@@ -0,0 +1,37 @@
+import { AlpacaClient } from "@master-chief/alpaca";
+import { addBusinessDays } from "date-fns";
+import fastify from "fastify";
+
+const client = new AlpacaClient({
+    credentials: {
+        key: process.env.ALPACA_SECRET_KEY_ID!,
+        secret: process.env.ALPACA_SECRET_KEY!,
+        paper: true,
+    },
+    rate_limit: false,
+});
+
+const server = fastify({
+    logger: true,
+    ignoreTrailingSlash: true,
+    bodyLimit: 1048576 * 100,
+});
+
+server.get("/", async (request, reply) => {
+    const bars = client.getBars({
+        symbol: "SPY",
+        start: addBusinessDays(Date.now(), -2),
+        end: new Date(),
+        timeframe: "1Min",
+    });
+
+    return bars;
+});
+
+server.listen(process.env.port || 8080, (err) => {
+    const serverAddress = server.server && server.server.address();
+    if (err || !serverAddress || typeof serverAddress === "string") {
+        server.log.error("uncaught error trying to init server", err);
+        process.exit(1);
+    }
+});
