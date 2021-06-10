556d13947e74676f5f0ea372deb5c219786df25e
neeschit <nischit14@gmail.com>
2021-06-09 22:39:32 -0400

    v1.1.0

--- core-data/src/cache.ts
+++ core-data/src/cache.ts
@@ -3,3 +3,7 @@ import { format } from "date-fns";
 export const getCacheKey = (key: string, epoch: number): string => {
     return `${key.toLowerCase()}_${format(epoch, "yyyy_MM_dd")}`;
 };
+
+export const getBoomRequestCacheKey = (epoch: number): string => {
+    return getCacheKey("boom_requested_count", epoch);
+};
